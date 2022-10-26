c7bd0f7c1ae0079976e2fb5a9165987bdcdd7b31
Colin Lam <colinl1995@gmail.com>
2022-10-26 22:01:04 +1100

    moved DB model to pydantic model conversion function to class method

--- api/data_structures.py
+++ api/data_structures.py
@@ -4,6 +4,8 @@ from enum import Enum
 from uuid import UUID
 from typing import Optional
 
+from db.models import Heroes
+
 
 class BaseSerialisableModel(BaseModel):
     def to_dict(self):
@@ -24,6 +26,13 @@ class Hero(BaseSerialisableModel):
     name: str
     randomable: bool = True
     hero_type: HeroType
+
+    @classmethod
+    def from_hero_db_model(cls, hero_row: Heroes):
+        return cls(id=hero_row.id,
+                   name=hero_row.name,
+                   randomable=hero_row.randomable,
+                   hero_type=HeroType(hero_row.hero_type))
 
 
 # The fields of this model should remain aligned with the fields in the Hero model
